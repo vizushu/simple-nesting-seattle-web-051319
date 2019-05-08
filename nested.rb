4147986aa284478e1e5b101844c5944aaf410597
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 04:51:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,13 +59,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-:yukihiro_matsumoto => {
-  :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-}
+
 
 	programmer_hash = 
- 		{
+	{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
