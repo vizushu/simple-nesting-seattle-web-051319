3a5d6902f7b940ccf28603683df0947035a5f1c2
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 04:58:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,6 +115,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
