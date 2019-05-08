2cd7357d89b5ebd4c95c0e608afed675dbcff1b4
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 04:50:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,10 +59,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}
 
 	programmer_hash = 
  		{
